83148011a6ae28734479b6b702cca6f49271fc87
Jackson kitsao <kitsaojackson22@gmail.com>
2026-06-30 16:36:55 +0300

    chore: switch catalogue

--- src/utils/testaudio.ts
+++ src/utils/testaudio.ts
@@ -7,10 +7,14 @@ export const catalogue = [
     //   name: "XFM Best of 2016 Part 1",
     //   src: "https://f005.backblazeb2.com/file/laniakea-cdn/examples/XFM+best+of+2016+part+1.mp3",
     // },
+    // {
+    //   name: "XFM Best of 2016 Part 2",
+    //   src: "https://f005.backblazeb2.com/file/laniakea-cdn/examples/XFM+best+of+2016+part+2.mp3.webm",
+    // },
     {
-      name: "XFM Best of 2016 Part 2",
-      src: "https://f005.backblazeb2.com/file/laniakea-cdn/examples/XFM+best+of+2016+part+2.mp3.webm",
-    },
+      name:"xfm",
+      src:"https://pub-d0b66de2ad994d6886b8137b63dc45c9.r2.dev/vod/xfm/XFM_1055_Kenyamp3_KLICKAUD%20(1).mp3"
+    }
   ];
  
   
